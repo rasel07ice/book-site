b0758a38f0f2f9e371995394afd73127b5b447ef
rasel07ice <rasel07ice@gmail.com>
2024-09-25 12:20:00 +0600

    second commit

--- src/components/book/BookList.jsx
+++ src/components/book/BookList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getAllBooks as getBooks } from "../../utils/book";
 import Book from "./Book";
 function BookList() {
